f6f4505394cb5f6d2d35a8b47f94ef8675477ce9
RemiSaurel <r.saurel5@gmail.com>
2023-04-10 19:12:01 +0200

    first commit

---
+++ src/main/java/fr/miage/MIAGELand/MiageLandApplication.java
@@ -0,0 +1,13 @@
+package fr.miage.MIAGELand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiageLandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiageLandApplication.class, args);
+	}
+
+}
