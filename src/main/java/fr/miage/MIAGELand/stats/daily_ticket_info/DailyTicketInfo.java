04f23f9d85303452066f06fd34c5a1c612712e11
RemiSaurel <r.saurel5@gmail.com>
2023-05-20 10:46:02 +0200

    add: refactoring ticketinfodata

---
+++ src/main/java/fr/miage/MIAGELand/stats/daily_ticket_info/DailyTicketInfo.java
@@ -0,0 +1,31 @@
+package fr.miage.MIAGELand.stats.daily_ticket_info;
+
+import fr.miage.MIAGELand.stats.TicketInfoData;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+public class DailyTicketInfo {
+
+    @Id
+    private String dayMonthYear;
+    private Long ticketCount;
+    private Long ticketPaidCount;
+    private Long ticketUsedCount;
+    private Long ticketCancelledCount;
+    private Double totalPrice;
+    private Double benefits;
+
+    public void updateData(DailyTicketInfoData dailyTicketInfoData) {
+        ticketCount += dailyTicketInfoData.getTicketCount();
+        ticketPaidCount += dailyTicketInfoData.getTicketCount();
+        totalPrice += dailyTicketInfoData.getTotalPrice();
+        benefits += dailyTicketInfoData.getBenefits();
+    }
+}
---
+++ src/main/java/fr/miage/MIAGELand/stats/daily_ticket_info/DailyTicketInfoData.java
@@ -0,0 +1,10 @@
+package fr.miage.MIAGELand.stats.daily_ticket_info;
+
+import fr.miage.MIAGELand.stats.TicketInfoData;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DailyTicketInfoData extends TicketInfoData {
+}
