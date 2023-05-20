a281415321f41f3a11813d3e71c4fb684479e7e9
RemiSaurel <r.saurel5@gmail.com>
2023-05-20 08:02:21 +0200

    add: swagger to test API

--- src/main/java/fr/miage/MIAGELand/employee/EmployeeRepository.java
+++ src/main/java/fr/miage/MIAGELand/employee/EmployeeRepository.java
@@ -5,8 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     Employee findByEmail(String email);
 
-    Employee findById(long id);
-    void deleteById(long id);
-
     long countByRole(EmployeeRole employeeRole);
 }
--- src/main/java/fr/miage/MIAGELand/ticket/TicketRepository.java
+++ src/main/java/fr/miage/MIAGELand/ticket/TicketRepository.java
@@ -9,12 +9,7 @@ import java.util.List;
 
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
 
-    Ticket findById(long id);
     List<Ticket> findAllByDateAfter(LocalDateTime now);
-    long countByState(TicketState ticketState);
-    long countAllByDateBetween(LocalDateTime begin, LocalDateTime end);
-    long countAllByDateBetweenAndState(LocalDateTime begin, LocalDateTime end, TicketState state);
-    float findPriceById(long id);
     Page<Ticket> findAll(Pageable pageable);
 
 }
