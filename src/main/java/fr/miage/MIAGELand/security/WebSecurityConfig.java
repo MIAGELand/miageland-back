8924645a96a71374d2ed7cecdd3a671bfa064b01
RemiSaurel <r.saurel5@gmail.com>
2023-05-30 09:56:08 +0200

    fix: allowed origins

--- src/main/java/fr/miage/MIAGELand/security/WebSecurityConfig.java
+++ src/main/java/fr/miage/MIAGELand/security/WebSecurityConfig.java
@@ -39,7 +39,7 @@ public class WebSecurityConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 if (Arrays.asList(environment.getActiveProfiles()).contains("dev")) {
                     registry.addMapping("/api/**")
-                            .allowedOrigins("http://127.0.0.1:5173")
+                            .allowedOrigins("http://localhost:5173", "http://127.0.0.1:5173")
                             .allowedMethods("*")
                             .allowCredentials(true); // Allow credentials (cookies) in the CORS response
                 }
